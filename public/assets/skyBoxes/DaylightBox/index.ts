eb45cbfbb1897420a5d41a1e36305389c94f542d
twoballs-ai <twoballs20@gmail.com>
2025-06-04 00:31:00 +0300

    fix

---
+++ public/assets/skyBoxes/DaylightBox/index.ts
@@ -0,0 +1,8 @@
+import posx from "../Daylight Box_Pieces/Daylight Box_Right.bmp";
+import negx from "../Daylight Box_Pieces/Daylight Box_Left.bmp";
+import posy from "../Daylight Box_Pieces/Daylight Box_Top.bmp";
+import negy from "../Daylight Box_Pieces/Daylight Box_Bottom.bmp";
+import posz from "../Daylight Box_Pieces/Daylight Box_Front.bmp";
+import negz from "../Daylight Box_Pieces/Daylight Box_Back.bmp";
+
+export const DaylightBoxPaths = { posx, negx, posy, negy, posz, negz }
